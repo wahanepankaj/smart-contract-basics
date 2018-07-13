4cb23d423c5789c08e31eb07eecc29b77197a875
Pankaj Wahane <pankajwahane@gmail.com>
2018-07-13 22:43:22 +0530

    Showing list of deployed contract

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
 import React, { Component } from 'react';
+import { Card, Button } from 'semantic-ui-react';
+import factory from '../ethereum/factory';
 
-export default () => {
+class CampaignIndex extends Component {
+
+  static async getInitialProps() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
+
+    return { campaigns };
+  }
+
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: (
+            <a>View Campaign</a>
+        ),
+        fluid: true
+      };
+    });
+
+    return <Card.Group items={items} />;
+  }
+
+  render() {
     return (
-        <h1>Welcome to show page!</h1>
+        <div>
+          <link
+            rel="stylesheet"
+            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+          />
+          <h3>Open Campaigns</h3>
+            
+          {this.renderCampaigns()}
+        </div>
     );
+  }
 }
+
+export default CampaignIndex;
